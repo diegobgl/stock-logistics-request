a3db3ad06a6975f14da8b9ea06a66e8a25031005
Diego Gajardo <deskodoo@a2group.cl>
2024-10-07 15:57:33 -0300

    location id filter

--- stock_request/models/stock_request_order.py
+++ stock_request/models/stock_request_order.py
@@ -228,33 +228,29 @@ class StockRequestOrder(models.Model):
         self.change_childs()
 
 
-#    @api.onchange('warehouse_id')
-#    def _onchange_warehouse_id(self):
-#        if self.warehouse_id:
-#            # Filtrar ubicaciones hijas de la bodega y de tipo 'internal' o 'transit'
-        #     return {
-        #         'domain': {
-        #             'location_id': [
-        #                 ('id', 'child_of', self.warehouse_id.view_location_id.id),
-        #                 ('usage', 'in', ['internal', 'transit'])
-        #             ]
-        #         }
-        #     }
-        # else:
-        #     # Si no hay bodega seleccionada, no mostrar ubicaciones
-        #     return {
-        #         'domain': {
-        #             'location_id': [('id', '=', False)]
-        #         }
-        #     }        
+    @api.onchange('warehouse_id')
+    def _onchange_warehouse_id(self):
+        if self.warehouse_id:
+            location_root_id = self.warehouse_id.view_location_id.id
+            print(f"Ubicación raíz de la bodega: {location_root_id}")  # Depuración
+            if location_root_id:
+                return {
+                    'domain': {
+                        'location_id': [
+                            ('id', 'child_of', location_root_id),
+                            ('usage', 'in', ['internal', 'transit'])
+                        ]
+                    }
+                }
+        
 
-    @api.onchange('location_id')
-    def onchange_location_id(self):
-         if self.location_id:
-             # Asignar la ubicación a todas las líneas de productos
-             for line in self.stock_request_ids:
-                 line.location_id = self.location_id
-         self.change_childs()
+    # @api.onchange('location_id')
+    # def onchange_location_id(self):
+    #      if self.location_id:
+    #          # Asignar la ubicación a todas las líneas de productos
+    #          for line in self.stock_request_ids:
+    #              line.location_id = self.location_id
+    #      self.change_childs()
 
 
     @api.onchange("procurement_group_id")
